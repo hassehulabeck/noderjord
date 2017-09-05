88d3718d7abbfcebd96a738610cca11e77dcbcc6
Hans Andersson <hans@hulabeck.se>
2017-09-05 09:44:26 +0200

    Added 3.4
    Tre filer som visar på hur man plockar parametrar och querys.

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-var mod = require('./module.js');
+let express = require('express');
+let app = express();
 
-mod.print();
-mod.printingHello();
+var users = require('./users.json');
+
+var findUser = function(userName, callback) {
+  if (!users[userName])
+    return callback(new Error(
+      'No user matching '
+       + userName
+      )
+    );
+  return callback(null, users[userName]);
+};
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Få värden genom params.
+app.get('/user/:userName', function(req, res){
+    var userName = req.params.userName;
+    findUser(userName, function(error, user)   {
+        if (error) 
+            return res.end("Fel: " + error);
+        return res.json(user);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Övning 3.4 lyssnar på 3000.");
+});
