745657f289177d23ba0249d6064b895cd69709c2
Hans Andersson <hans@hulabeck.se>
2017-09-08 13:27:36 +0200

    Ny lösning för att skriva förnamn på pristagare.

--- 1.8/app.js
+++ 1.8/app.js
@@ -1,17 +1,26 @@
 // Nobel laureates
 const https = require('https');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
 var data = "";
 https.get('https://yhjust16.herokuapp.com/nobel', (res) => {
+    app.listen(3000, () => {
+        console.log('Alive at 3000');
+    });
     res.on('data', (d) => {
         data += (d);
     });
     res.on('end', () => {
         let parsed = JSON.parse(data);
-        for (let person of parsed) {
-            if (person.prizes[0].category == 'literature') // Visa enbart litt-pristagarna.
-                console.log(person.prizes[0].year + "... " + person.firstname + " " + person.surname);
-        }
+        app.get('/', (req, res) => {
+            res.render('index', {
+                title: 'Yahoooo',
+                list: parsed
+            })
+        });
     });
-}).on('error', (e) => {
-   console.error(e); 
-});
+})
